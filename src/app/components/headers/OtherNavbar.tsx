0e2dbbf4b45a504cc35c474fce104c1ff6b0d4e0
Kevin <kazakov28082003@gmail.com>
2026-06-29 08:41:11 +0900

    feat: publish other navbar header componentn

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -1,3 +1,79 @@
+import { Container, Stack, Box, Button } from "@mui/material";
+import { NavLink } from "react-router-dom";
+
 export function OtherNavbar() {
-  return <h2>OtherNavbar</h2>;
+  const authmember = null;
+  return (
+    <div className="other-navbar">
+      <Container className="navbar-container">
+        <Stack className="menu">
+          <Box>
+            <NavLink to="/">
+              <img className="brand-logo" src="/icons/burak.svg" />
+            </NavLink>
+          </Box>
+          <Stack className="links">
+            <Box className={"hover-line"}>
+              <NavLink to="/">Home</NavLink>
+            </Box>
+            <Box className={"hover-line"}>
+              <NavLink to="/products" activeClassName="underline">
+                Products
+              </NavLink>
+            </Box>
+            {authmember ? (
+              <Box className={"hover-line"}>
+                <NavLink to="/orders" activeClassName="underline">
+                  Orders
+                </NavLink>
+              </Box>
+            ) : null}
+            {authmember ? (
+              <Box className={"hover-line"}>
+                <NavLink to="/member-page" activeClassName="underline">
+                  My Page
+                </NavLink>
+              </Box>
+            ) : null}
+            <Box className={"hover-line"}>
+              <NavLink to="/help" activeClassName="underline">
+                Help
+              </NavLink>
+            </Box>
+            {/*BASKET*/}
+            {!authmember ? (
+              <Box>
+                <Button variant="contained" className="login-button">
+                  Login
+                </Button>
+              </Box>
+            ) : (
+              <img
+                className="user-avatar"
+                src="/icons/default-user.svg"
+                aria-haspopup={"true"}
+              />
+            )}
+          </Stack>
+        </Stack>
+        {/* <Stack className="header-frame">
+          <Stack className="detail">
+            <Box className="head-main-txt">World's Most Delicious Cousine</Box>
+            <Box className="wel-txt">The Choice, not just a choice</Box>
+            <Box className="service-txt">24 hours service</Box>
+            <Box className="signup">
+              {!authmember ? (
+                <Button variant="contained" className="signup-btn">
+                  SIGN UP
+                </Button>
+              ) : null}
+            </Box>
+          </Stack>
+          <Stack className="logo-frame">
+            <div className="logo-img"></div>
+          </Stack>
+        </Stack> */}
+      </Container>
+    </div>
+  );
 }
