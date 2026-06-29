c898bfb61ba5502b0729ae77e905295ae824db48
Kevin <kazakov28082003@gmail.com>
2026-06-30 07:07:03 +0900

    feat: organize homepage related components

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -1,5 +1,19 @@
-import { Container } from "@mui/material";
+import PopularDishes from "./PopularDishes";
+import NewDishes from "./NewDishes";
+import Advertisement from "./Advertisement";
+import ActiveUsers from "./ActiveUsers";
+import Events from "./Events";
+import Statistics from "./Statisticss";
 
 export default function HomePage() {
-  return <Container>Home page</Container>;
+  return (
+    <div className="home-page">
+      <Statistics />
+      <PopularDishes />
+      <NewDishes />
+      <Advertisement />
+      <ActiveUsers />
+      <Events />
+    </div>
+  );
 }
