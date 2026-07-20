fe431bdb2f7e9e4cce7513230be00695321a8c4d
Kevin <kazakov28082003@gmail.com>
2026-07-21 02:41:48 +0900

    feat: create config and env variables

---
+++ src/lib/config.ts
@@ -0,0 +1,8 @@
+const serverApi: string = `${process.env.REACT_APP_API_URL}`;
+export const messages = {
+  error1: "Something went wrong!",
+  error2: "Please logn first",
+  error3: "Please fullfil all inputs",
+  error4: "Message is empty",
+  error5: "Only images with jpeg, jpg, png format allowed!",
+};
